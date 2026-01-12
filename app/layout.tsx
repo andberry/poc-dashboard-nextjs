cf4d315b8b447f2289c8867f6674d60b94f38295
Andrea Berardi <andrea.berardi@verbinteractive.com>
2026-01-12 18:01:19 +0100

    Working on fonts, css, images

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,12 @@
+import "@/app/ui/global.css"
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={'font-inter antialiased'}>
       <body>{children}</body>
     </html>
   );
---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import {Inter} from 'next/font/google'
+import { Space_Grotesk } from 'next/font/google'
+
+export const fontInter = Inter({subsets: ['latin']})
+export const fontSpaceGrotesk = Space_Grotesk({subsets: ['latin']})
