fb37e1366dd1f04ac7ec0c8db8d0aefd6598b305
Andrea Berardi <andrea.berardi@verbinteractive.com>
2026-01-18 00:12:27 +0100

    Working on Dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,23 @@
-export default function Page() {
-  return <div>This is Dashboard Page</div>
+import { fetchRevenueData } from "@/lib/data/api";
+import { RevenueData } from "@/ui/components/RevenueData";
+
+export default async function Page() {
+  const revenueData = await fetchRevenueData();
+  return (
+    <div className="bg-slate-200 h-full p-12">
+      <h1 className="font-sg text-4xl font-bold mb-8 uppercase">
+        Dashboard Homepage
+      </h1>
+      <div className="grid grid-cols-2 gap-12">
+        {revenueData && (
+          <div className="border border-slate-300 rounded-sm p-8">
+            <RevenueData data={revenueData} />
+          </div>
+        )}
+        <div className="border border-slate-300 rounded-sm p-8">
+          Latest Invoice Here
+        </div>
+      </div>
+    </div>
+  );
 }
