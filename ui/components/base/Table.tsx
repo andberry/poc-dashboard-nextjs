4817997941ccbb57577cb805db3b3458100b0d53
Andrea Berardi <andrea.berardi@verbinteractive.com>
2026-01-18 01:02:56 +0100

    Adding base Table component

---
+++ ui/components/base/Table.tsx
@@ -0,0 +1,27 @@
+type TBodyRow = string[];
+
+interface IProps {
+  headerRows: string[];
+  bodyRows: TBodyRow[];
+}
+
+export const Table = ({ headerRows, bodyRows }: IProps) => (
+  <table className="border-collapse table-auto w-full">
+    <thead>
+      <tr className="border border-slate-300 text-left bg-teal-600 text-white">
+        {headerRows.map((item) => (
+          <th className="font-sg px-3 py-2 w-1/3 min-w-[100px]">{item}</th>
+        ))}
+      </tr>
+    </thead>
+    <tbody>
+      {bodyRows.map((row) => (
+        <tr>
+          {row.map((item) => (
+            <td className="px-3 py-2 border border-slate-300">{item}</td>
+          ))}
+        </tr>
+      ))}
+    </tbody>
+  </table>
+);
