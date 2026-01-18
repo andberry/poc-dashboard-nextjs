a82bc7745d38a08b2c80f3f60d245873f97cf2e3
Andrea Berardi <andrea.berardi@verbinteractive.com>
2026-01-18 01:10:30 +0100

    Layout improvements

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -6,13 +6,13 @@ interface IProps {
   title?: string;
 }
 
-const DashboardLayout = ({title, children}: PropsWithChildren<IProps>) => (
-   <div className="flex min-h-screen flex-col md:flex-row bg-slate-100">
-      <div className="w-full flex-none md:w-64">
-        <SideNav />
-      </div>
-      <div className="grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+const DashboardLayout = ({ title, children }: PropsWithChildren<IProps>) => (
+  <div className="flex min-h-screen flex-col md:flex-row bg-slate-100">
+    <div className="w-full flex-none md:w-64">
+      <SideNav />
     </div>
-)
+    <div className="grow p-4 md:overflow-y-auto">{children}</div>
+  </div>
+);
 
 export default DashboardLayout;
