2463c3d294ce874a7bd6698cf038de9bf96e9382
Andrea Berardi <andrea.berardi@verbinteractive.com>
2026-01-18 01:55:52 +0100

    Fixing missing key in Table component

--- ui/components/base/Table.tsx
+++ ui/components/base/Table.tsx
@@ -9,16 +9,20 @@ export const Table = ({ headerRows, bodyRows }: IProps) => (
   <table className="border-collapse table-auto w-full">
     <thead>
       <tr className="border border-slate-300 text-left bg-teal-600 text-white">
-        {headerRows.map((item) => (
-          <th className="font-sg px-3 py-2 w-1/3 min-w-[100px]">{item}</th>
+        {headerRows.map((item, index) => (
+          <th key={index} className="font-sg px-3 py-2 w-1/3 min-w-[100px]">
+            {item}
+          </th>
         ))}
       </tr>
     </thead>
     <tbody>
-      {bodyRows.map((row) => (
-        <tr>
-          {row.map((item) => (
-            <td className="px-3 py-2 border border-slate-300">{item}</td>
+      {bodyRows.map((row, rowIndex) => (
+        <tr key={rowIndex}>
+          {row.map((item, itemIndex) => (
+            <td key={itemIndex} className="px-3 py-2 border border-slate-300">
+              {item}
+            </td>
           ))}
         </tr>
       ))}
